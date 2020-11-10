be1ec3513de5ba9a33f012015a87081ff3a7f3a7
akoval <c@apple456>
2020-11-10 17:46:24 +0300

    ik.

--- story/resources/fs/python/ckp/mq/mq/factory.py
+++ story/resources/fs/python/ckp/mq/mq/factory.py
@@ -2,12 +2,15 @@ class Provider:
     def create_producer(self, conf):
         pass
 
+    def create_consumer(self, conf):
+        pass
+
 
 class Connection:
     def send(self, topic, key, value):
         pass
 
-    def pool(self):
+    def pool(self, topics, group_id, to):
         pass
 
 
@@ -29,14 +32,46 @@ class KafkaConnection(Connection):
         producer.produce(topic, key=key, value=value)
         producer.poll(1)
 
-    def pool(self):
-        pass
+    def pool(self, topics, group_id, to):
+        import time
+        conf = {
+            "bootstrap.servers": self.urls,
+            "group.id": group_id,
+            "auto.offset.reset": "smallest"
+        }
+        consumer = self.kafka_provider.create_consumer(conf)
+        consumer.subscribe(topics)
+        records = []
+        start_time = time.time()
+        try:
+            while True:
+                msg = consumer.poll(timeout=1.0)
+                current_time = time.time()
+                t = current_time - start_time
+                if t > to:
+                    break
+                if msg is None:
+                    continue
+                if msg.error():
+                    raise Exception(msg.error())
+                else:
+                    k = msg.key()
+                    v = msg.value()
+                    record = (k, v)
+                    records.append(record)
+        except:
+            pass
+        return records
 
 
 class KafkaProvider(Provider):
     def create_producer(self, conf):
         from confluent_kafka import Producer
         return Producer(conf)
+
+    def create_consumer(self, conf):
+        from confluent_kafka import Consumer
+        return Consumer(conf)
 
 
 class KafkaConnectionFactory(ConnectionFactory):
@@ -56,17 +91,29 @@ class Factory:
         return self.connection_factory.create_connection()
 
 
-def kafka_simple_send():
+def kafka_factory():
     provider = KafkaProvider()
     connection_factory = KafkaConnectionFactory(provider, "localhost:9092")
-    factory = Factory(connection_factory)
+    return Factory(connection_factory)
 
+
+def kafka_simple_send():
+    factory = kafka_factory()
     connection = factory.create_connection()
     connection.send("q-data", "42", "73")
 
 
+def kafka_simple_pool():
+    factory = kafka_factory()
+    connection = factory.create_connection()
+    records = connection.pool(["q-data"], "42", 10)
+    for r in records:
+        print(r)
+
+
 def main():
-    kafka_simple_send()
+    # kafka_simple_send()
+    kafka_simple_pool()
 
 
 if __name__ == "__main__":
--- story/resources/fs/python/ckp/mq/tests/factory_test.py
+++ story/resources/fs/python/ckp/mq/tests/factory_test.py
@@ -4,6 +4,21 @@ from unittest.mock import create_autospec
 from ..mq import factory as mq
 
 
+class MessageMock:
+    def __init__(self, k, v):
+        self.k = k
+        self.v = v
+
+    def key(self):
+        return self.k
+
+    def value(self):
+        return self.v
+
+    def error(self):
+        return False
+
+
 class TestFactory(unittest.TestCase):
 
     def test_send(self):
@@ -27,6 +42,32 @@ class TestFactory(unittest.TestCase):
         connection.send(topic, key, value)
         producers[0].produce.assert_called_with(topic, key=key, value=value)
         producers[0].poll.assert_called_with(1)
+
+    def test_pool(self):
+        topics = ["test"]
+
+        key = "42"
+        value = "73"
+
+        consumers = []
+
+        def create_consumer(conf):
+            from confluent_kafka import Consumer
+            mock = create_autospec(Consumer)
+            mock.poll.return_value = MessageMock(key, value)
+            consumers.append(mock)
+            return mock
+
+        provider = create_autospec(mq.Provider)
+        provider.create_consumer = create_consumer
+        connection_factory = mq.KafkaConnectionFactory(provider, "")
+        factory = mq.Factory(connection_factory)
+        connection = factory.create_connection()
+        records = connection.pool(topics, "test", 1)
+        self.assertTrue(len(records) > 0)
+        record = records[0]
+        self.assertTrue(record[0] == "42")
+        self.assertTrue(record[1] == "73")
 
 
 if __name__ == "__main__":
