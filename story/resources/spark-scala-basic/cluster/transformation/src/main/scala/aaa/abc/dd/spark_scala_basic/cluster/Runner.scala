0a1fee8e539dbc01343e1340936b688185fa956a
akoval <c@apple456>
2020-12-22 19:22:02 +0300

    k.

--- story/resources/spark-scala-basic/cluster/transformation/src/main/scala/aaa/abc/dd/spark_scala_basic/cluster/Runner.scala
+++ story/resources/spark-scala-basic/cluster/transformation/src/main/scala/aaa/abc/dd/spark_scala_basic/cluster/Runner.scala
@@ -1,14 +1,21 @@
 package aaa.abc.dd.spark_scala_basic.cluster
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Runner {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().master("local[2]").enableHiveSupport().getOrCreate()
-    val rdd = spark.sparkContext.parallelize(Seq(1, 2, 3, 4, 5))
-    val rddCollected = rdd.collect()
+    import spark.implicits._
+
+    val data = Seq((1, 2, 0), (3, 4, 0), (7, 8, 1))
+    val df = data.toDF("x1", "x2", "label")
+    val result = run(df)
     // scalastyle:off println
-    rddCollected.foreach(println)
+    println(result)
     // scalastyle:on
+  }
+
+  def run(df: DataFrame): Double = {
+    df.groupBy().sum().collect().flatMap(r => r.toSeq).map(i => i.toString.toDouble).sum
   }
 }
