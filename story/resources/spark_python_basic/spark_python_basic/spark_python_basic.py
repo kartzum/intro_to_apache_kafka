3c69304269533b7fdcd4d7c7b9a57608aa8b7b9f
kartzum <kartzum@gmail.com>
2020-12-22 21:46:06 +0300

    ik.

--- story/resources/spark_python_basic/spark_python_basic/spark_python_basic.py
+++ story/resources/spark_python_basic/spark_python_basic/spark_python_basic.py
@@ -16,15 +16,26 @@ def logging_exception(msg):
     logging.getLogger().exception(msg)
 
 
+def convert(items):
+    for value in items:
+        for element in value:
+            yield element
+
+
+def run(df):
+    return sum(convert(df.groupBy().sum().collect()))
+
+
 def main(args):
     result = 0
     spark = None
     try:
         logging_init()
         spark = SparkSession.builder.enableHiveSupport().getOrCreate()
-        rdd = spark.sparkContext.parallelize([1, 2, 3, 4, 5])
-        rdd_collected = rdd.collect()
-        logging_info(rdd_collected)
+        data = [(1, 2, 0), (3, 4, 0), (7, 8, 1)]
+        df = spark.createDataFrame(data=data, schema=["x1", "x2", "label"])
+        r = run(df)
+        logging_info(r)
     except Exception as e:
         logging_exception(e)
         result = 1
