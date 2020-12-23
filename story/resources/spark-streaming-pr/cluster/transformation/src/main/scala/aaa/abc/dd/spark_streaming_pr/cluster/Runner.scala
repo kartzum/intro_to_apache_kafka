01291b8b704f9e6b4cc8b0cbc623c872fe670f3a
akoval <c@apple456>
2020-12-23 10:01:23 +0300

    k.

---
+++ story/resources/spark-streaming-pr/cluster/transformation/src/main/scala/aaa/abc/dd/spark_streaming_pr/cluster/Runner.scala
@@ -0,0 +1,21 @@
+package aaa.abc.dd.spark_streaming_pr.cluster
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object Runner {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().master("local[2]").enableHiveSupport().getOrCreate()
+    import spark.implicits._
+
+    val data = Seq((1, 2, 0), (3, 4, 0), (7, 8, 1))
+    val df = data.toDF("x1", "x2", "label")
+    val result = run(df)
+    // scalastyle:off println
+    println(result)
+    // scalastyle:on
+  }
+
+  def run(df: DataFrame): Double = {
+    df.groupBy().sum().collect().flatMap(r => r.toSeq).map(i => i.toString.toDouble).sum
+  }
+}
---
+++ story/resources/spark-streaming-pr/cluster/transformation/src/test/scala/aaa/abc/dd/spark_streaming_pr/cluster/RunnerSuite.scala
@@ -0,0 +1,27 @@
+package aaa.abc.dd.spark_streaming_pr.cluster
+
+import org.apache.spark.sql.SparkSession
+import org.scalatest.{Matchers, Outcome, fixture}
+
+class RunnerSuite extends fixture.FunSuite with Matchers {
+  test("run") { spark =>
+    import spark.implicits._
+
+    val data = Seq((1, 2, 0), (3, 4, 0), (7, 8, 1))
+    val df = data.toDF("x1", "x2", "label")
+
+    val result = Runner.run(df)
+
+    assert(result == 26.0)
+  }
+
+  override protected def withFixture(test: OneArgTest): Outcome = {
+    val spark = SparkSession.builder().master("local[2]").getOrCreate()
+
+    try withFixture(test.toNoArgTest(spark))
+
+    finally spark.stop()
+  }
+
+  override type FixtureParam = SparkSession
+}
