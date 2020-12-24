0a0e78b7bb4b84ae831059860581ac0561f0a11f
kartzum <kartzum@gmail.com>
2020-12-24 10:12:09 +0300

    ik.

--- story/resources/spark-streaming-pr/cluster/transformation/src/main/scala/aaa/abc/dd/spark_streaming_pr/cluster/AService.scala
+++ story/resources/spark-streaming-pr/cluster/transformation/src/main/scala/aaa/abc/dd/spark_streaming_pr/cluster/AService.scala
@@ -2,38 +2,55 @@ package aaa.abc.dd.spark_streaming_pr.cluster
 
 import org.apache.spark.mllib.linalg
 import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.dstream.{DStream, InputDStream}
 import org.apache.spark.mllib.regression.{LabeledPoint, StreamingLinearRegressionWithSGD}
+import org.apache.spark.sql.Row
 
 object AService {
 
   trait Train {
-    def train(ds: DStream[LabeledPoint]): Unit
+    def train(ds: InputDStream[Row]): Unit
   }
 
   trait Predict {
-    def predict(ds: DStream[(Double, linalg.Vector)]): Unit
+    def predict(ds: InputDStream[Row]): Unit
   }
 
   class AService extends Train with Predict {
     var model: StreamingLinearRegressionWithSGD = _
 
-    def train(ds: DStream[LabeledPoint]): Unit = {
+    def train(ds: InputDStream[Row]): Unit = {
+      val dsLabeled: DStream[LabeledPoint] =
+        ds.map(r => {
+          val values = r.toSeq
+          val label = values.head.asInstanceOf[Double]
+          val features = values.map(i => i.asInstanceOf[Double]).tail.toArray
+          LabeledPoint(label, Vectors.dense(features))
+        })
+
       model = new StreamingLinearRegressionWithSGD()
         .setInitialWeights(Vectors.zeros(3))
         .setNumIterations(2)
         .setStepSize(0.1)
         .setMiniBatchFraction(0.25)
 
-      model.trainOn(ds)
+      model.trainOn(dsLabeled)
 
     }
 
-    def predict(ds: DStream[(Double, linalg.Vector)]): Unit = {
-      val r: DStream[(Double, Double)] = model.predictOnValues(ds)
+    def predict(ds: InputDStream[Row]): Unit = {
+      val dsLabeled: DStream[(Double, linalg.Vector)] =
+        ds.map(r => {
+          val values = r.toSeq
+          val label = values.head.asInstanceOf[Double]
+          val features = values.map(i => i.asInstanceOf[Double]).tail.toArray
+          (label, Vectors.dense(features))
+        })
+
+      val r: DStream[(Double, Double)] = model.predictOnValues(dsLabeled)
 
       r.foreachRDD(d => {
-        d.foreach(v => {
+        d.collect().foreach(v => {
           // scalastyle:off println
           println("Results: " + v._1 + ": " + v._2)
           // scalastyle:on println
--- story/resources/spark-streaming-pr/cluster/transformation/src/test/scala/aaa/abc/dd/spark_streaming_pr/cluster/AServiceSuite.scala
+++ story/resources/spark-streaming-pr/cluster/transformation/src/test/scala/aaa/abc/dd/spark_streaming_pr/cluster/AServiceSuite.scala
@@ -1,10 +1,7 @@
 package aaa.abc.dd.spark_streaming_pr.cluster
 
-import org.apache.spark.mllib.linalg
-import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Row, SparkSession}
 import org.apache.spark.rdd.RDD
-import org.apache.spark.mllib.linalg.Vectors
 import org.scalatest.{Matchers, Outcome, fixture}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.dstream.InputDStream
@@ -17,21 +14,21 @@ class AServiceSuite extends fixture.FunSuite with Matchers {
 
     val service = new AService.AService()
 
-    val trainData = mutable.Queue[RDD[LabeledPoint]]()
-    val trainDStream: InputDStream[LabeledPoint] = streamingContext.queueStream(trainData)
+    val trainData = mutable.Queue[RDD[Row]]()
+    val trainDStream: InputDStream[Row] = streamingContext.queueStream(trainData)
 
     service.train(trainDStream)
 
-    val predictData = mutable.Queue[RDD[(Double, linalg.Vector)]]()
-    val predictDStream: InputDStream[(Double, linalg.Vector)] = streamingContext.queueStream(predictData)
+    val predictData = mutable.Queue[RDD[Row]]()
+    val predictDStream: InputDStream[Row] = streamingContext.queueStream(predictData)
 
     service.predict(predictDStream)
 
     streamingContext.start()
 
-    val t1 = spark.sparkContext.makeRDD(Seq(LabeledPoint(1.0, Vectors.dense(0.1, 0.2, 0.3))))
-    val t2 = spark.sparkContext.makeRDD(Seq(LabeledPoint(2.0, Vectors.dense(0.0, 0.0, 0.3))))
-    val t3 = spark.sparkContext.makeRDD(Seq(LabeledPoint(3.0, Vectors.dense(0.0, 0.2, 0.3))))
+    val t1 = spark.sparkContext.makeRDD(Seq(Row(1.0, 0.1, 0.2, 0.3)))
+    val t2 = spark.sparkContext.makeRDD(Seq(Row(2.0, 0.0, 0.0, 0.3)))
+    val t3 = spark.sparkContext.makeRDD(Seq(Row(3.0, 0.0, 0.2, 0.3)))
 
     trainData += t1
     trainData += t2
@@ -39,13 +36,15 @@ class AServiceSuite extends fixture.FunSuite with Matchers {
 
     Thread.sleep(2000)
 
-    val p1 = spark.sparkContext.makeRDD(Seq((1.0, Vectors.dense(0.1, 0.2, 0.3))))
-    val p2 = spark.sparkContext.makeRDD(Seq((2.0, Vectors.dense(0.3, 0.1, 0.2))))
+    val p1 = spark.sparkContext.makeRDD(Seq(Row(1.0, 0.1, 0.2, 0.3)))
+    val p2 = spark.sparkContext.makeRDD(Seq(Row(2.0, 0.3, 0.1, 0.2)))
+
+    Thread.sleep(2000)
 
     predictData += p1
     predictData += p2
 
-    streamingContext.awaitTerminationOrTimeout(7000)
+    streamingContext.awaitTerminationOrTimeout(10000)
   }
 
   override protected def withFixture(test: OneArgTest): Outcome = {
