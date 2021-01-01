5d40214c31d0cfdb36bd6c4b15e3444e9bc7ecb2
kartzum <kartzum@gmail.com>
2021-01-01 11:47:31 +0300

    ik.

--- story/resources/spark-streaming-pr/cluster/transformation/src/main/scala/aaa/abc/dd/spark_streaming_pr/cluster/StructuredStreamingService.scala
+++ story/resources/spark-streaming-pr/cluster/transformation/src/main/scala/aaa/abc/dd/spark_streaming_pr/cluster/StructuredStreamingService.scala
@@ -1,9 +1,41 @@
 package aaa.abc.dd.spark_streaming_pr.cluster
 
+import org.apache.spark.ml.regression.{LinearRegression, LinearRegressionModel}
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.streaming.StreamingQuery
+
 class StructuredStreamingService {
-  def train(): Unit = {
+  var service: LinearRegression = _
+  var model: LinearRegressionModel = _
+
+  def train(ds: DataFrame): Unit = {
+    service = new LinearRegression().setMaxIter(10).setRegParam(0.01)
+    model = service.fit(ds)
   }
 
-  def predict(): Unit = {
+  def predict(ds: DataFrame): StreamingQuery = {
+
+    val m = ds.sparkSession.sparkContext.broadcast(model)
+
+    def transformFun(features: org.apache.spark.ml.linalg.Vector): Double = {
+      m.value.predict(features)
+    }
+
+    val transform: org.apache.spark.ml.linalg.Vector => Double = transformFun
+
+    val toUpperUdf = udf(transform)
+
+    val predictionDs = ds.withColumn("prediction", toUpperUdf(ds("features")))
+
+    predictionDs
+      .writeStream
+      .foreachBatch((r: DataFrame, i: Long) => {
+        r.show()
+        // scalastyle:off println
+        println(s"$i")
+        // scalastyle:on println
+      })
+      .start()
   }
 }
--- story/resources/spark-streaming-pr/cluster/transformation/src/test/scala/aaa/abc/dd/spark_streaming_pr/cluster/StructuredStreamingServiceSuite.scala
+++ story/resources/spark-streaming-pr/cluster/transformation/src/test/scala/aaa/abc/dd/spark_streaming_pr/cluster/StructuredStreamingServiceSuite.scala
@@ -1,12 +1,37 @@
 package aaa.abc.dd.spark_streaming_pr.cluster
 
+import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.execution.streaming.MemoryStream
 import org.scalatest.{Matchers, Outcome, fixture}
 
 class StructuredStreamingServiceSuite extends fixture.FunSuite with Matchers {
-  /*test("run") { spark =>
-
-  }*/
+  test("run") { spark =>
+    // Prepare data.
+    val trainDs = spark.createDataFrame(Seq(
+      (6.0, Vectors.dense(1.0, 1.0)),
+      (8.0, Vectors.dense(1.0, 2.0)),
+      (9.0, Vectors.dense(2.0, 2.0)),
+      (11.0, Vectors.dense(2.0, 3.0))
+    )).toDF("label", "features")
+    // Create model and train.
+    val service = new StructuredStreamingService()
+    service.train(trainDs)
+    // Predict and results.
+    implicit val sqlCtx = spark.sqlContext
+    import spark.implicits._
+    val source = MemoryStream[Record]
+    source.addData(Record(1.0, Vectors.dense(3.0, 5.0)))
+    source.addData(Record(2.0, Vectors.dense(4.0, 6.0)))
+    val predictDs = source.toDF()
+    service.predict(predictDs).awaitTermination(2000)
+    // +-----+---------+------------------+
+    // |label| features|        prediction|
+    // +-----+---------+------------------+
+    // |  1.0|[3.0,5.0]|15.966990887541273|
+    // |  2.0|[4.0,6.0]|18.961384020443553|
+    // +-----+---------+------------------+
+  }
 
   override protected def withFixture(test: OneArgTest): Outcome = {
     val spark = SparkSession.builder().master("local[2]").getOrCreate()
@@ -17,4 +42,7 @@ class StructuredStreamingServiceSuite extends fixture.FunSuite with Matchers {
   }
 
   override type FixtureParam = SparkSession
+
+  case class Record(label: Double, features: org.apache.spark.ml.linalg.Vector)
+
 }
